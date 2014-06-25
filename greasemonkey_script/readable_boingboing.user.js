d32d4e97ac4927e4d2dfb3f7404db430cb010dfe
rajbot <raj@archive.org>
2014-06-25 14:20:42 -0700

    Add programatically-generated greasemonkey script.

---
+++ greasemonkey_script/readable_boingboing.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name           Readable Boing Boing
+// @version        1.1
+// @author         rajbot
+// @description    Improve readability of boingboing.net by increasing the width of the Posts column and decreasing the width of the Features column.
+// @grant          GM_addStyle
+// @include        http://boingboing.net/
+// ==/UserScript==
+
+// This script was generated programatically by chromeext_to_greasemonkey.py
+
+(function() {
+    GM_addStyle(@media screen and (min-width:1020px) {    #posts-loop.home {        max-width: 620px !important;    }    #features-loop {        max-width: 320px !important;    }}@media screen and (min-width:728px) and (max-width:1020px) {    #posts-loop.home {        width: 60% !important;        max-width: 620px !important;    }    #features-loop.home {        width: calc(35% - 20px) !important;    }}#ad_leaderboard {    display: none !important;});
+
+})();
