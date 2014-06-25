09077b3b90925fa1d392ad4085e12a0757f7e6c2
rajbot <raj@archive.org>
2014-06-25 14:28:59 -0700

    Put css string in quotes.

--- greasemonkey_script/readable_boingboing.user.js
+++ greasemonkey_script/readable_boingboing.user.js
@@ -10,6 +10,6 @@
 // This script was generated programatically by chromeext_to_greasemonkey.py
 
 (function() {
-    GM_addStyle(@media screen and (min-width:1020px) {    #posts-loop.home {        max-width: 620px !important;    }    #features-loop {        max-width: 320px !important;    }}@media screen and (min-width:728px) and (max-width:1020px) {    #posts-loop.home {        width: 60% !important;        max-width: 620px !important;    }    #features-loop.home {        width: calc(35% - 20px) !important;    }}#ad_leaderboard {    display: none !important;});
+    GM_addStyle('@media screen and (min-width:1020px) {    #posts-loop.home {        max-width: 620px !important;    }    #features-loop {        max-width: 320px !important;    }}@media screen and (min-width:728px) and (max-width:1020px) {    #posts-loop.home {        width: 60% !important;        max-width: 620px !important;    }    #features-loop.home {        width: calc(35% - 20px) !important;    }}#ad_leaderboard {    display: none !important;}');
 
 })();
