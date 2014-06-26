933b57f943b7eeacc760afec383dbb12b7a1bb30
rajbot <raj@archive.org>
2014-06-26 12:27:42 -0700

    Remove .advertisement class

--- greasemonkey_script/readable_boingboing.user.js
+++ greasemonkey_script/readable_boingboing.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Readable Boing Boing
-// @version        1.1
+// @version        1.2
 // @author         rajbot
 // @description    Improve readability of boingboing.net by increasing the width of the Posts column and decreasing the width of the Features column.
 // @grant          GM_addStyle
@@ -10,6 +10,6 @@
 // This script was generated programatically by chromeext_to_greasemonkey.py
 
 (function() {
-    GM_addStyle('@media screen and (min-width:1020px) {    #posts-loop.home {        max-width: 620px !important;    }    #features-loop {        max-width: 320px !important;    }}@media screen and (min-width:728px) and (max-width:1020px) {    #posts-loop.home {        width: 60% !important;        max-width: 620px !important;    }    #features-loop.home {        width: calc(35% - 20px) !important;    }}#ad_leaderboard {    display: none !important;}');
+    GM_addStyle('@media screen and (min-width:1020px){#posts-loop.home{max-width:620px!important}#features-loop{max-width:320px!important}}@media screen and (min-width:728px) and (max-width:1020px){#posts-loop.home{width:60%!important;max-width:620px!important}#features-loop.home{width:calc(35% - 20px)!important}}#ad_leaderboard{display:none!important}.advertisement{display:none!important}');
 
 })();
